0dc379a74e6798808dcddb406ae4ace55f4b58d5
Marco Chiapello <chiapello.m@gmail.com>
2024-05-24 09:33:29 +0200

    Allen U13U 4

--- _scripts/nuovoAllenamento.R
+++ _scripts/nuovoAllenamento.R
@@ -2,7 +2,7 @@
 ### Folder
 d <- "05/23/2024"
 date <- lubridate::mdy(d)
-team <- "U13F"
+team <- "U13U"
 n <- 4
 commento <- c("")
 ### File
@@ -10,18 +10,18 @@ categories <- c(team, "2024-2025", "Pre-season")
 # convocate <- c("Per-Giu", "Ger-Val", "Bon-Isa", "Tap-Ann", "Chi-Mar",
 #                "Ser-Sof", "Del-Aur", "Cir-Ade", "Tor-Ari", "Aud-Gin",
 #                "Fra-Mat", "Lam-Gre", "Cel-Sar")
-convocate <- c("Per-Giu", "Del-Aur", "Gil-Ari", "Pan-Mar",
-               "Neg-Ire", "Tap-Ann", "Ber-Sil", "Cir-Ade",
-               "Bon-Isa", "Goy-Bea", "Ger-Val", "Tor-Ari")
-# convocate <- c("Pan-Mar", "Urs-Ann", "Agu-Bia", "Gil-Ari", "Neg-Ire",
-#                "Goy-Bea", "Mol-Gin", "Fio-Mat", "Cas-Giu", "Col-Ann",
-#                "Bud-Eri", "Ber-Sil")
-assenti <- c("Del-Aur")
+# convocate <- c("Per-Giu", "Del-Aur", "Gil-Ari", "Pan-Mar",
+#                "Neg-Ire", "Tap-Ann", "Ber-Sil", "Cir-Ade",
+#                "Bon-Isa", "Goy-Bea", "Ger-Val", "Tor-Ari")
+convocate <- c('Chi-Mar', 'Bud-Eri', 'Aud-Gin', 'Lam-Gre', 'Cel-Sar', 'Fra-Mat', 
+               'Urs-Ann', 'Agu-Bia', 'Mol-Gin', 'Fio-Mat', 'Cas-Giu', 'Col-Ann', 
+               'Ser-Sof')
+assenti <- c("Cas-Giu")
 vincitori <- c()
 impegno <- 0.8
-obiettivo <- 0.9
+obiettivo <- 0.8
 obiettivi <- "Fase gioco"
-url <- "https://youtu.be/6QobzOa57L0"
+url <- NA
 
 # Prepare the folder
 dd <- lubridate::wday(date, label = TRUE)
