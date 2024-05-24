fc0b0fdfafbfc68fda93090753c2972c52e168b3
Marco Chiapello <chiapello.m@gmail.com>
2024-05-24 07:23:20 +0200

    Scout rice

--- scout/allenamenti/2024-05-20/rice.R
+++ scout/allenamenti/2024-05-20/rice.R
@@ -52,3 +52,54 @@ ric |>
     )
   )
 
+
+
+
+xrecep <- px |> 
+  filter(skill == "Reception")
+
+se <- px |> 
+  filter(skill == "Set", lag(skill) == "Reception") 
+
+
+px |> 
+  group_by(point_id) |> 
+  filter(skill %in% c("Reception", "Set")) |> 
+  count(point_id) |> filter(n < 2)
+
+
+
+ggplot(xrecep, aes(start_coordinate_x, start_coordinate_y,
+                    xend = end_coordinate_x, yend = end_coordinate_y, colour = evaluation)) +
+  geom_segment(arrow = arrow(length = unit(2, "mm"), type = "closed", angle = 20)) +
+  scale_colour_manual(values = c(Ace = "limegreen", Error = "firebrick", Other = "dodgerblue"),
+                      name = "Evaluation") +
+  ggcourt()
+
+
+px %>% 
+  mutate(set_coordinate_x = case_when(skill == "Reception" ~ lead(start_coordinate_x)),
+              set_coordinate_y = case_when(skill == "Reception" ~ lead(start_coordinate_y))) %>%
+  mutate(evaluation2 = case_when(skill == "Reception" & evaluation %in% c("Negative, limited attack",
+                                                   "Poor, no attack") ~ "Negative",
+                                 skill == "Reception" & evaluation %in% c("OK, no first tempo possible",
+                                                   "Positive, attack") ~ "Positive",
+                                 skill == "Reception" & TRUE ~ evaluation)) |> 
+  filter(player_id == "Per-Giu") |> 
+  ggplot(aes(end_coordinate_x, end_coordinate_y, xend = set_coordinate_x, yend = set_coordinate_y, colour = evaluation2)) +
+  geom_segment(arrow = arrow(length = unit(2, "mm"), type = "closed", angle = 20)) + ggcourt() +
+  facet_wrap(vars(end_zone))
+
+px %>% 
+  mutate(set_coordinate_x = case_when(skill == "Reception" ~ lead(start_coordinate_x)),
+         set_coordinate_y = case_when(skill == "Reception" ~ lead(start_coordinate_y))) %>%
+  mutate(evaluation2 = case_when(skill == "Reception" & evaluation %in% c("Negative, limited attack",
+                                                                          "Poor, no attack") ~ "Negative",
+                                 skill == "Reception" & evaluation %in% c("OK, no first tempo possible",
+                                                                          "Positive, attack") ~ "Positive",
+                                 skill == "Reception" & TRUE ~ evaluation)) |> 
+  filter(player_id == "Cir-Ade") |> 
+  ggplot(aes(set_coordinate_x, set_coordinate_y, 
+             colour = evaluation2)) +
+  geom_point() + ggcourt() +
+  facet_wrap(vars(end_zone))
