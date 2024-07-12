b76e8150417eb853de67e9a1e1d6c2ad88357171
Marco Chiapello <chiapello.m@gmail.com>
2024-07-12 13:34:53 +0200

    scout

--- scout/partite/2024-05-30_BassoCanaveseVolley_U13U-vs-Fortitudo/scout.R
+++ scout/partite/2024-05-30_BassoCanaveseVolley_U13U-vs-Fortitudo/scout.R
@@ -17,6 +17,7 @@ ov_scouter(dir_ls(paste0(here(), out), regexp = 'ovs$'),
            scouting_options = list(transition_sets = TRUE,
            attack_table = read_csv(paste0(here(), '/data/myAttacks.csv'))),
            app_styling = list(review_pane_width = 50),
+           # scout_mode = "type",
            launch_browser = TRUE)
 
 ################################################################################
